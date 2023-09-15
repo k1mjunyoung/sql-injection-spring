49c9cac638523f7d506c00391427376c799a342d
k1mjunyoung <vovrwm@gmail.com>
2023-09-15 12:05:54 +0900

    update: spring initializr

---
+++ src/main/java/sqlinjectionspring/SqlInjectionSpringApplication.java
@@ -0,0 +1,13 @@
+package sqlinjectionspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlInjectionSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlInjectionSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sqlinjectionspring/SqlInjectionSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package sqlinjectionspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlInjectionSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
