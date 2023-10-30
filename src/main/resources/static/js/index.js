3cae2d2b06b8a9c9933e7b679c490d036cad275b
GeonwooShin <rjsdnslawkd@naver.com>
2023-10-30 18:30:11 +0900

    refactor: 상태 값에 따라 response handling

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -9,16 +9,25 @@ document.getElementById("submitForm").addEventListener("submit", async (e) => {
     method: "POST",
     body: formData,
   })
-    .then((response) => response.json())
+    .then((response) => {
+      if (response.status === 200) {
+        return response.json();
+      } else if (response.status === 400) {
+        throw new Error("잘못된 SQL 문법입니다.");
+      } else {
+        throw new Error("알 수 없는 에러입니다. 나중에 다시 시도하세요.");
+      }
+    })
     .then((datas) => {
       if (datas) {
         datas.forEach((data) => {
-          console.log(data);
           resultParagraph.innerText += JSON.stringify(data) + "\n";
         });
+      } else {
+        resultParagraph.innerText += "데이터가 존재하지 않습니다.";
       }
     })
     .catch((err) => {
-      window.alert(err);
+      window.alert(err.message);
     });
 });
